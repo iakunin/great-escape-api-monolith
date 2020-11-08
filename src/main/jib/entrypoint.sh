8582da4c84391175f9d17a950c979fdcd092e59c
iakunin <iakunin@users.noreply.github.com>
2020-11-08 17:13:11 +0300

    GreatEscapeBackend -> GreatEscapeApiMonolith renaming

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -2,4 +2,4 @@
 
 #echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.greatescape.backend.GreatEscapeBackendApp"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.greatescape.api.monolith.ApiMonolithApp"  "$@"
