68c95167b4f0a96b8b890ab7b9c93b58ab59660c
Maksim Iakunin <yakuninm+github@gmail.com>
2020-12-26 21:13:12 +0300

    Fixing mat.view

--- src/main/resources/config/liquibase/changelog/20201219212339_added_QuestAggregation.sql
+++ src/main/resources/config/liquibase/changelog/20201219212339_added_QuestAggregation.sql
@@ -2,16 +2,17 @@
 --changeset jhipster:20201219212339_1
 create materialized view quest_aggregation as
 select q.*,
-    c.discount_in_percents,
+    tmp_slot.discount_in_percents,
     tmp_slot.min_price
 from quest q
-join company c on q.company_id = c.id
 join (
-    select sl.quest_id, min(sl.price) as min_price
+    select sl.quest_id,
+        max(sl.discount_in_percents) as discount_in_percents,
+        min(sl.price) as min_price
     from slot sl
     group by sl.quest_id
 ) as tmp_slot on tmp_slot.quest_id = q.id
-group by q.id, c.discount_in_percents, tmp_slot.min_price
+group by q.id, tmp_slot.discount_in_percents, tmp_slot.min_price
 ;
 
 --rollback drop materialized view "quest_aggregation";
