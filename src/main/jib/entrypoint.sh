3c6b3f78779f4d99e10c00ab9970369f48b5c1a8
Maksim Iakunin <yakuninm+github@gmail.com>
2021-01-12 21:44:57 +0300

    Getting rid of JHIPSTER_SLEEP env-var

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-#echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.greatescape.api.monolith.ApiMonolithApp"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch \
+    -Djava.security.egd=file:/dev/./urandom \
+    -cp /app/resources/:/app/classes/:/app/libs/* \
+    "com.greatescape.api.monolith.ApiMonolithApp" \
+     "$@"
