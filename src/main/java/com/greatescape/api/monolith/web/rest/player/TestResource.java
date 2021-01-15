c982ac8d79c04816f5342381440ae5c90cade7d4
Maksim Iakunin <yakuninm+github@gmail.com>
2021-01-15 17:03:26 +0300

    Extracting sendpulse-password to CloudSecret

---
+++ src/main/java/com/greatescape/api/monolith/web/rest/player/TestResource.java
@@ -0,0 +1,25 @@
+package com.greatescape.api.monolith.web.rest.player;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/player-api")
+@RequiredArgsConstructor
+@Slf4j
+public class TestResource {
+
+    @Value("${app.test}")
+    private final String secret;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return new ResponseEntity<>(secret, HttpStatus.OK);
+    }
+}
