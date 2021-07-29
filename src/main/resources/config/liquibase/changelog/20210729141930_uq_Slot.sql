f393885ff6fee19cc3b0511327ef0bde2290e64b
Maksim Iakunin <yakuninm+github@gmail.com>
2021-07-29 15:33:34 +0300

    Changing unique index on slot.date_time_local and slot.date_time_with_time_zone

--- src/main/resources/config/liquibase/changelog/20210729141930_uq_Slot.sql
+++ src/main/resources/config/liquibase/changelog/20210729141930_uq_Slot.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 --changeset jhipster:20210729141930_1
 
-create unique index ux_slot_date_time_local
-    on slot (date_time_local);
+create unique index ux__slot__quest_id__date_time_local
+    on slot (quest_id, date_time_local);
 
-create unique index ux_slot_date_time_with_time_zone
-    on slot (date_time_with_time_zone);
+create unique index ux__slot__quest_id__date_time_with_time_zone
+    on slot (quest_id, date_time_with_time_zone);
